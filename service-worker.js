cc40772d03fc9b1c33a0346b9c57adffb0bdb576
NesTTy <nestt.yhr@gmail.com>
2024-08-15 17:26:17 +0300

    54

--- service-worker.js
+++ service-worker.js
@@ -3,17 +3,17 @@ self.addEventListener('install', function(event) {
     self.skipWaiting();
 });
 
-// self.addEventListener('activate', function(event) {
-//     console.log('Service Worker activating.');
-//     event.waitUntil(clients.claim());
-//     event.waitUntil(
-//         clients.matchAll({ type: 'window' }).then(windowClients => {
-//             for (let client of windowClients) {
-//                 client.navigate(client.url);
-//             }
-//         })
-//     );
-// });
+self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating.');
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        clients.matchAll({ type: 'window' }).then(windowClients => {
+            for (let client of windowClients) {
+                client.navigate(client.url);
+            }
+        })
+    );
+});
 
 self.addEventListener('fetch', function(event) {
     console.log('Fetching:', event.request.url);
