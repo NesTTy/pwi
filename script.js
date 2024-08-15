7f70b876185141c5acb869e0ade1fab4deec46b8
NesTTy <nestt.yhr@gmail.com>
2024-08-15 17:16:19 +0300

    revers

--- script.js
+++ script.js
@@ -21,51 +21,13 @@ installBtn.addEventListener('click', (e) => {
     });
 });
 
-// Push notifications button
-const subscribeBtn = document.getElementById('subscribeBtn');
-
-subscribeBtn.addEventListener('click', async () => {
-    if ('serviceWorker' in navigator) {
-        const registration = await navigator.serviceWorker.ready;
-        const permission = await Notification.requestPermission();
-        if (permission === 'granted') {
-            const subscription = await registration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array('YOUR_VAPID_PUBLIC_KEY')
-            });
-            console.log('Push subscription:', JSON.stringify(subscription));
-            // Update button text or state to indicate subscription success
-            subscribeBtn.textContent = 'Push Notifications Enabled';
-            subscribeBtn.disabled = true; // Disable the button after successful subscription
-            // Send subscription to your server
-        } else {
-            subscribeBtn.textContent = 'Enable Push Notifications';
-        }
-    }
-});
-
-function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/');
-
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
-}
-
 // Service Worker registration
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
         .then(function(registration) {
             console.log('Service Worker registered with scope:', registration.scope);
             if (registration.installing) {
-            //    window.location.href = "YOUR_DESIRED_URL";
+               // window.location.href = "YOUR_DESIRED_URL";
             }
         }).catch(function(error) {
             console.log('Service Worker registration failed:', error);
