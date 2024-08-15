ad5d23b32e668518ad48b9b5ca9b7fc8e2d9d581
NesTTy <nestt.yhr@gmail.com>
2024-08-15 22:54:00 +0300

    asg

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installing.');
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activating.');
-    event.waitUntil(clients.claim());
-    event.waitUntil(
-        clients.matchAll({ type: 'window' }).then(windowClients => {
-            for (let client of windowClients) {
-                client.navigate(client.url);
-            }
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    console.log('Fetching:', event.request.url);
-});
+self.addEventListener("install", () => {
+    return true;
+  });
+  
+  self.addEventListener("activate", () => {
+    return true;
+  });
+  
+  self.addEventListener("sync", () => {
+    return true;
+  });
+  
+  self.addEventListener("fetch", () => {
+    return true;
+  });
+  
+  self.addEventListener("push", function (event) {
+      
+  });
+  
